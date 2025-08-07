7ff2b03b5611f489d415cc870d39f8a2d077811e
Pongsan <pongsan@amazon.com>
2025-08-07 14:38:36 +0800

    Add Qwen3 8B Neuron

--- components/llm-model/tgi/index.mjs
+++ components/llm-model/tgi/index.mjs
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url";
 import path from "path";
 import fs from "fs";
 import handlebars from "handlebars";
-import { $ } from "zx";
+import { $, cd } from "zx";
 $.verbose = true;
 
 export const name = "TGI";
@@ -26,6 +26,7 @@ export async function install() {
 
   await $`kubectl apply -f ${path.join(DIR, "namespace.yaml")}`;
   await $`kubectl apply -f ${path.join(DIR, "pvc-huggingface-cache.yaml")}`;
+  await $`kubectl apply -f ${path.join(DIR, "pvc-neuron-cache.yaml")}`;
   const secretTemplatePath = path.join(DIR, "secret.template.yaml");
   const secretRenderedPath = path.join(DIR, "secret.rendered.yaml");
   const secretTemplateString = fs.readFileSync(secretTemplatePath, "utf8");
@@ -35,7 +36,35 @@ export async function install() {
   };
   fs.writeFileSync(secretRenderedPath, secretTemplate(secretVars));
   await $`kubectl apply -f ${secretRenderedPath}`;
-  const { models } = config["llm-model"]["tgi"];
+  const { models, enableNeuron } = config["llm-model"]["tgi"];
+  if (enableNeuron) {
+    // Need until this PR merged and new image available
+    // https://github.com/huggingface/text-generation-inference/pull/3308
+    // ghcr.io/huggingface/text-generation-inference
+    try {
+      await $`kubectl -n tgi get job tgi-neuron-build`;
+      console.log("TGI Neuron build job already exists, skipping build...");
+    } catch (err) {
+      await utils.terraform.apply(DIR);
+      const ecrRepoUrl = await utils.terraform.output(DIR, { outputName: "ecr_repository_url" });
+      cd(DIR);
+      const jobTemplatePath = path.join(DIR, `job-tgi-neuron-build.template.yaml`);
+      const jobRenderedPath = path.join(DIR, `job-tgi-neuron-build.rendered.yaml`);
+      const jobTemplateString = fs.readFileSync(jobTemplatePath, "utf8");
+      const jobTemplate = handlebars.compile(jobTemplateString);
+      const jobVars = { IMAGE: `${ecrRepoUrl}:latest` };
+      fs.writeFileSync(jobRenderedPath, jobTemplate(jobVars));
+      await $`kubectl -n tgi delete job tgi-neuron-build --ignore-not-found=true`;
+      await $`kubectl apply -f ${jobRenderedPath}`;
+      console.log("Waiting for TGI Neuron build job to complete, up to 30 mins...");
+      try {
+        await $`kubectl -n tgi wait --for=condition=complete job tgi-neuron-build --timeout=1800s`;
+        console.log("TGI Neuron build job completed successfully!");
+      } catch (err) {
+        console.error("TGI Neuron build job did not complete in time or failed:", err);
+      }
+    }
+  }
   await utils.model.addModels(models, "llm-model", "tgi");
 }
 
@@ -44,5 +73,6 @@ export async function uninstall() {
   await utils.model.removeAllModels(models, "llm-model", "tgi");
   await $`kubectl delete -f ${path.join(DIR, "secret.rendered.yaml")} --ignore-not-found`;
   await $`kubectl delete -f ${path.join(DIR, "pvc-huggingface-cache.yaml")} --ignore-not-found`;
+  await $`kubectl delete -f ${path.join(DIR, "pvc-neuron-cache.yaml")} --ignore-not-found`;
   await $`kubectl delete -f ${path.join(DIR, "namespace.yaml")} --ignore-not-found`;
 }
---
+++ components/llm-model/tgi/main.tf
@@ -0,0 +1,73 @@
+variable "region" {
+  type    = string
+  default = "us-west-2"
+}
+variable "name" {
+  type    = string
+  default = "genai-on-eks"
+}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.96.0"
+    }
+  }
+}
+provider "aws" {
+  region = var.region
+}
+locals {
+  name = "tgi-neuron"
+}
+resource "aws_ecr_repository" "this" {
+  name                 = "${var.name}-${local.name}"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "KMS"
+  }
+}
+output "ecr_repository_url" {
+  value = aws_ecr_repository.this.repository_url
+}
+
+module "pod_identity" {
+  source  = "terraform-aws-modules/eks-pod-identity/aws"
+  version = "1.12.0"
+
+  name                 = "${var.name}-${var.region}-tgi-neuron-build"
+  use_name_prefix      = false
+  attach_custom_policy = true
+  policy_statements = [
+    {
+      actions = [
+        "ecr:GetAuthorizationToken",
+      ]
+      resources = ["*"]
+    },
+    {
+      actions = [
+        "ecr:CompleteLayerUpload",
+        "ecr:UploadLayerPart",
+        "ecr:InitiateLayerUpload",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:PutImage",
+        "ecr:BatchGetImage"
+      ]
+      resources = [aws_ecr_repository.this.arn]
+    }
+  ]
+  associations = {
+    litellm = {
+      service_account = "tgi-neuron-build"
+      namespace       = "tgi"
+      cluster_name    = var.name
+    }
+  }
+}
